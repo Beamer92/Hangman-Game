aa5dee502cd9c78ceb34f770ec41522c8547e964
Brendan Woodell <brendanw@rainier.com>
2017-10-04 16:09:21 -0700

    Finished WordGen, copying to master hangman docuemtn for merge with master

--- Hangman.py
+++ Hangman.py
@@ -1,4 +1,9 @@
 from graphics import *
+import requests
+import keys
+import json
+import random
+
 win = GraphWin('Hangman', 500, 500)
 
 def noose():
@@ -23,6 +28,23 @@ def noose():
     
     brace = Line(Point(415,80), Point(385, 50))
     brace.draw(win) 
-    
-noose()    
-    
+       
+
+def wordgen():
+    #One way to do without an API, produces words nobody would know 99% of the time
+    #word = requests.get('http://setgetgo.com/randomword/get.php').text
+    #print(word)
+
+    #uses API key from wordnik, grabs 26 words between 7-10 characters that have at least 15 dictionary entries(?) so they're knowable words
+    #randomly select number between 0-25 and returns that word
+    ker = keys.api_key
+    urlstring = "http://api.wordnik.com/v4/words.json/randomWords?minCorpusCount=10000&minDictionaryCount=15&excludePartOfSpeech=proper-noun,proper-noun-plural,proper-noun-posessive,suffix,family-name,idiom,affix&hasDictionaryDef=true&includePartOfSpeech=noun,verb,adjective,definite-article,conjunction&limit=26&maxLength=10&minLength=7&api_key="
+    callstring = urlstring + ker
+    response = requests.get(callstring)
+    wordlist =  response.json()
+    wordnum = random.randint(0,25)
+    wordchoice = wordlist[wordnum]['word']
+    return (wordchoice)
+
+noose() 
+wordgen()
